d4532a5b498ac23477528e2b4d0cf19e17a13f17
BunnyEars <anoddone.anoddone@gmail.com>
2019-10-23 17:47:55 -0500

    1.12.8.1 added support for nvarchar, char, nchar and varbinary data types

--- Constructor.cs
+++ Constructor.cs
@@ -43,7 +43,7 @@ namespace DataFramework {
         public enum dbLog { Where, And, Or }
 
         /// <summary>Tipos de datos</summary>
-        public enum dbTyp { Int, Varchar, Date, DateTime, Decimal, Bit, Custom }
+        public enum dbTyp { Int, Varchar, NVarchar, Char, NChar, Date, DateTime, Decimal, Bit, Varbinary, Custom }
 
         /// <summary>Var size</summary>
         public enum dbSiz { Max, Other }
